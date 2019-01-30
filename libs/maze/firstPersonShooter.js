08949ebce08030e92024ac659ee70224ffd7bdfb
skylarkingmonkey <SkylarkingMonkey@users.noreply.github.com>
2019-01-30 14:18:57 -0800

    gettin it ta work in bits and pieces

--- libs/maze/firstPersonShooter.js
+++ libs/maze/firstPersonShooter.js
@@ -7,46 +7,8 @@ var crate, crateTexture, crateNormalMap, crateBumpMap;
 
 var keyboard = {};
 var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
-var USE_WIREFRAME = false;
 
-var loadingScreen = {
-	scene: new THREE.Scene(),
-	camera: new THREE.PerspectiveCamera(90, 1280/720, 0.1, 100),
-	box: new THREE.Mesh(
-		new THREE.BoxGeometry(0.5,0.5,0.5),
-		new THREE.MeshBasicMaterial({ color:0x4444ff })
-	)
-};
-var loadingManager = null;
-var RESOURCES_LOADED = false;
 
-// Models index
-var models = {
-	tent: {
-		obj:"models/Tent_Poles_01.obj",
-		mtl:"models/Tent_Poles_01.mtl",
-		mesh: null
-	},
-	campfire: {
-		obj:"models/Campfire_01.obj",
-		mtl:"models/Campfire_01.mtl",
-		mesh: null
-	},
-	pirateship: {
-		obj:"models/Pirateship.obj",
-		mtl:"models/Pirateship.mtl",
-		mesh: null
-	},
-	uzi: {
-		obj:"models/uziGold.obj",
-		mtl:"models/uziGold.mtl",
-		mesh: null,
-		castShadow:false
-	}
-};
-
-// Meshes index
-var meshes = {};
 
 
 function init(){
@@ -69,22 +31,6 @@ function init(){
 	};
 	
 	
-	mesh = new THREE.Mesh(
-		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
-	);
-	mesh.position.y += 1;
-	mesh.receiveShadow = true;
-	mesh.castShadow = true;
-	scene.add(mesh);
-	
-	meshFloor = new THREE.Mesh(
-		new THREE.PlaneGeometry(20,20, 10,10),
-		new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
-	);
-	meshFloor.rotation.x -= Math.PI / 2;
-	meshFloor.receiveShadow = true;
-	scene.add(meshFloor);
 	
 	
 	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
@@ -97,21 +43,6 @@ function init(){
 	light.shadow.camera.far = 25;
 	scene.add(light);
 	
-	
-	var textureLoader = new THREE.TextureLoader(loadingManager);
-	crateTexture = textureLoader.load("crate0/crate0_diffuse.jpg");
-	crateBumpMap = textureLoader.load("crate0/crate0_bump.jpg");
-	crateNormalMap = textureLoader.load("crate0/crate0_normal.jpg");
-	
-	crate = new THREE.Mesh(
-		new THREE.BoxGeometry(3,3,3),
-		new THREE.MeshPhongMaterial({
-			color:0xffffff,
-			map:crateTexture,
-			bumpMap:crateBumpMap,
-			normalMap:crateNormalMap
-		})
-	);
 	scene.add(crate);
 	crate.position.set(2.5, 3/2, 2.5);
 	crate.receiveShadow = true;
--- libs/maze/maze.js
+++ libs/maze/maze.js
@@ -1,5 +1,6 @@
        
             var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
+            var keyboard = {};
 
             var scene = new THREE.Scene();
 
@@ -9,7 +10,7 @@
 
 
             scene.background = new THREE.Color( 0xffffff );
-            scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+            scene.fog = new THREE.Fog( 0xffffff, 0, 350 );
             var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
             light.position.set( 0.5, 1, 0.75 );
             scene.add( light );            
@@ -17,71 +18,7 @@
             //var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 500 );  
             const camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
            // const camera = new THREE.PerspectiveCamera(70, 2, 1, 500);
-            camera.lookAt(new THREE.Vector3(0,player.height,0));
-         
-            controls = new THREE.PointerLockControls( camera );
-            //skipped a bunch of blocker instructions bs
-            scene.add( controls.getObject() );
-            var onKeyDown = function ( event ) {
-                switch ( event.keyCode ) {
-                    case 38:
-                    case 87:
-                        moveForward = true;
-                        break;
-                    case 37:
-                    case 65:
-                        turnLeft = true;
-                        break;
-                    case 40:
-                    case 83:
-                        moveBackward = true;
-                        break;
-                    case 39:
-                    case 68:
-                        turnRight = true;
-                        break;
-                    case 32: 
-                        if (canJump === true ) velocity.y += 350;
-                        canJump = false;
-                        break;
-
-                }
-            };
-            var onKeyUp = function ( event ) {
-                switch ( event.keyCode ) {
-                    case 38:
-                    case 87:
-                        moveForward = false;
-                        break;
-                    case 37:
-                    case 65:
-                        turnLeft = false;
-                        break;
-                    case 40:
-                    case 83:
-                        moveBackward = false;
-                    case 39:
-                    case 68:
-                        turnRight = false;
-                        break;
-
-                }
-            };
-            document.addEventListener( 'keydown', onKeyDown, false );
-            document.addEventListener( 'keyup', onKeyUp, false );
-            raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, -1, 0), 0, 1 );             
-
-
-           
-            //scene.rotation.set(  );
-       //     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-       //     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-       //     var cube = new THREE.Mesh( geometry, material );
-       //     scene.add( cube );
-
-      //      scene.rotation.z = ;
-      //      scene.rotation.x = ;
-      //      scene.rotation.y = ;
+            camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
             function resizeCanvasToDisplaySize(){
                 const canvas = renderer.domElement;
@@ -103,28 +40,25 @@
             var container = document.getElementById('viewport');
             container.appendChild( renderer.domElement );
 
-
-            const circle = document.createElement("div");
+////Joystick for Mobile/////
+        /*    const circle = document.createElement("div");
             circle.style.cssText = "position:absolute; bottom:55px; width:70px; height:70px; background:rgba(126, 126, 126, 0.5); border:#fff solid medium; border-radius:50%; left:50%; transform:translateX(-50%);";
             const thumb = document.createElement("div");
             thumb.style.cssText = "position: absolute; left: 18px; top: 18px; width: 35px; height: 35px; border-radius: 50%; background: #fff;";
             circle.appendChild(thumb);
             container.appendChild(circle);            
-
+*/
 
             var controls = new THREE.OrbitControls( camera, renderer.domElement );
             controls.enableDamping = true;
             controls.dampingFactor = 0.25;
             controls.enableZoom = true;
             controls.enableRotate = true;
-            camera.position.set(561, -8, 55);            
-            camera.rotation.set(-2.8, 1.5, 2.5);
-            controls.target = new THREE.Vector3(-20, 0, 0);
+
+            controls.target = new THREE.Vector3(560, -7, 60);
+            camera.position.set(580, -9.4, 60);
+            camera.rotation.set(-.6, 1.39, .6);
             controls.update();
-
-
-
-//cam pos x -7 y 13 z -7
 
             controls.keys = {
                 LEFT: 37, //left arrow
@@ -132,6 +66,21 @@
                 RIGHT: 39, // right arrow
                 BOTTOM: 40 // down arrow
             } 
+
+
+            
+
+
+            function keyDown(event){
+                keyboard[event.keyCode] = true;
+            }
+
+            function keyUp(event){
+                keyboard[event.keyCode] = false;
+            }
+
+            window.addEventListener('keydown', keyDown);
+            window.addEventListener('keyup', keyUp);
 
 
             var loader = new THREE.GLTFLoader();
@@ -144,61 +93,55 @@
             loader.load(
                 './libs/maze/MazeScaledUp.gltf',
                 function ( gltf ) {
-
                     scene.add( gltf.scene );
-
                   //  gltf.animations; // Array<THREE.AnimationClip>
                     gltf.scene; // THREE.Scene
                     gltf.scenes; // Array<THREE.Scene>
                 //    gltf.cameras; // Array<THREE.Camera>
                     gltf.asset; // Object
-
                 },
                 // called while loading is progressing
                 function ( xhr ) {
-
                     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
                 },
                 // called when loading has errors
                 function ( error ) {
-
                     console.log( 'An error happened' );
-
-    
                 }
-
             );
 
-
-
-    
-
-
-
             function animate(time) {
+                if(keyboard[87]){ // W key
+                    camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+                    camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+                }
+                if(keyboard[83]){ // S key
+                    camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+                    camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+                }
+                if(keyboard[65]){ // A key
+                    camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+                    camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+                }
+                if(keyboard[68]){ // D key
+                    camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+                    camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+                }
+                
+                if(keyboard[37]){ // left arrow key
+                    camera.rotation.y -= player.turnSpeed;
+                }
+                if(keyboard[39]){ // right arrow key
+                    camera.rotation.y += player.turnSpeed;
+                }
+                
                 time *= 0.001;
                 resizeCanvasToDisplaySize();
-
-                //scene.rotation.x += 0.01;
-
-                
                 requestAnimationFrame(animate);
-                
-                renderer.render(scene, camera);
-
                 controls.update();
+                renderer.render(scene, camera);
             }
 
             requestAnimationFrame(animate);
 
-          /*  var animate = function () {
-                requestAnimationFrame( animate );
-
-                    scene.rotation.x += 0.01;
-      //          cube.rotation.y += 0.01;
-
-                renderer.render( scene, camera );
-            };
-
-            animate();    */
+   
