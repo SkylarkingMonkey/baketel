3cf1407cf71e02882364bdd421ac8aa6a1463258
Jean-Marc Dykes <jean-marcdykes@JMBD.local>
2023-09-07 17:01:49 -0700

    reboot branch commit

--- resources/javascript/redHerring.js
+++ resources/javascript/redHerring.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the span with class "gateway"
-    const gatewaySpan = document.querySelector('.gateway');
-    // Check if the span exists
-    if (gatewaySpan) {
-      // Add click event listener to the span
-      gatewaySpan.addEventListener('click', function () {
-        // Change the body style to overflow: scroll
-        document.body.style.overflow = 'scroll';
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Select the span with class "gateway"
+//     const gatewaySpan = document.querySelector('.gateway');
+//     // Check if the span exists
+//     if (gatewaySpan) {
+//       // Add click event listener to the span
+//       gatewaySpan.addEventListener('click', function () {
+//         // Change the body style to overflow: scroll
+//         document.body.style.overflow = 'scroll';
   
-        // Select the div with class "red-herring"
-        const redHerringDiv = document.querySelector('.red-herring');
-        // Check if the div exists
-        if (redHerringDiv) {
-          // Change the div style to display: none
-          redHerringDiv.style.display = 'none';
-        }
-      });
-    }
-  });
+//         // Select the div with class "red-herring"
+//         const redHerringDiv = document.querySelector('.red-herring');
+//         // Check if the div exists
+//         if (redHerringDiv) {
+//           // Change the div style to display: none
+//           redHerringDiv.style.display = 'none';
+//         }
+//       });
+//     }
+//   });
